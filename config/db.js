ccd2143b27c968efc2cdd27fab3bb90304da6a7b
SAIKIRAN RANGU <144669094+SAIKIRANRANGUSAI@users.noreply.github.com>
2025-09-11 15:48:32 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 
 // Create a pool for promise-based queries
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "46.250.225.169",
-  user: process.env.DB_USER || "demo_colormo_usr",
-  password: process.env.DB_PASS || "QRdKdVpp3pnNhXBt",
-  database: process.env.DB_NAME || "vidya_vedika",
+  host: "46.250.225.169",
+  user: "demo_colormo_usr",
+  password: "QRdKdVpp3pnNhXBt",
+  database: "vidya_vedika",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
@@ -24,4 +24,5 @@ const db = mysql.createPool({
 })();
 
 module.exports = db;
+
 
