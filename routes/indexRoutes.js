83f7386e533820b237da0e5744f1ce4d94bada4f
SAIKIRAN RANGU <144669094+SAIKIRANRANGUSAI@users.noreply.github.com>
2025-09-17 12:19:37 +0530

    Update indexRoutes.js

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -64,13 +64,22 @@ router.get("/", async (req, res) => {
       heading: "Default Gallery Heading",
       description: "Default gallery description.",
     };
+    const [aboutRows] = await db.query("SELECT * FROM about_us WHERE id = 1");
+    let aboutUs = aboutRows[0] || {
+      heading: "Default About Us Heading",
+      paragraph1: "Default paragraph 1 about our school.",
+      paragraph2: "Default paragraph 2 about our school.",
+      image1: "/images/default1.png",
+      image2: "/images/default2.png",
+    };
+
     
 
     console.log("👉 CTA Content from DB:", ctaContent);
     console.log("👉 Counters from DB:", counters);
 
     // --- Pass both into EJS ---
-    res.render("index", { homeContent, ctaContent, counters, galleryContent, galleryImages, testimonialContent, testimonials, whyChooseUs,sections, courses });
+    res.render("index", { homeContent, ctaContent, counters, galleryContent, galleryImages, testimonialContent, testimonials, whyChooseUs, sections, courses, aboutUs });
 
   } catch (err) {
     console.error("Error in homepage route:", err);
@@ -79,3 +88,4 @@ router.get("/", async (req, res) => {
 });
 
 module.exports = router;
+
